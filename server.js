02117dd48f17b015b695194eb54ca3af6465875d
Oscar Campos <oscarc691@gmail.com>
2023-03-16 13:24:49 -0700

    set up what seems to look like a good code to start with and hopefully function

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/public/assets', express.static(__dirname + '/public/assets'));
@@ -15,5 +16,4 @@ require('./RouteFiles/Html-routes')(app);
 app.listen(PORT, function() {
     console.log('Application listening localhost' + PORT);
 });
-
 
