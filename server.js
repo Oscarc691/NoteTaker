acb2a1a834b4ac1176abe4fb025ee75faa2beb73
Oscar Campos <oscarc691@gmail.com>
2023-03-16 18:35:12 -0700

    have added to code tp point of almost function i believe

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/public/assets', express.static(__dirname + '/public/assets'));
@@ -16,4 +15,5 @@ require('./RouteFiles/Html-routes')(app);
 app.listen(PORT, function() {
     console.log('Application listening localhost' + PORT);
 });
+
 
