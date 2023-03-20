8708f6638bd4ecb2fb6bf3da5057c8851dd0d236
Oscar Campos <oscarc691@gmail.com>
2023-03-19 20:28:23 -0700

    reworked a couple folders since for some reason i couldnt get it to work, going to continue working on it but i think ive almost got it

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/public/assets', express.static(__dirname + '/public/assets'));
+app.use(express.static('public'));
 
 require('./RouteFiles/API-route')(app);
 require('./RouteFiles/Html-routes')(app);
 
 app.listen(PORT, function() {
-    console.log('Application listening localhost' + PORT);
+    console.log('Application listening http://localhost:' + PORT);
 });
 
 
